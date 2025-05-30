8dc7aeded9fba8f1b315116bdd038aed07bf9cd1
STEPHEN EDWARDS <stephen.denis.edwards@googlemail.com>
2025-05-30 16:43:27 +0100

    Adds code from sample app - compiles .net 9

---
+++ tingler-sign-in-maui/MSALClient/DownstreamApiHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace tingler_sign_in_maui.MSALClient
+{
+    public class DownstreamApiHelper
+    {
+        private string[] DownstreamApiScopes;
+        public DownStreamApiConfig DownstreamApiConfig;
+        private MSALClientHelper MSALClient;
+
+        public DownstreamApiHelper(DownStreamApiConfig downstreamApiConfig, MSALClientHelper msalClientHelper)
+        {
+            if (msalClientHelper == null)
+            {
+                throw new ArgumentNullException(nameof(msalClientHelper));
+            }
+
+            this.DownstreamApiConfig = downstreamApiConfig;
+            this.MSALClient = msalClientHelper;
+            this.DownstreamApiScopes = this.DownstreamApiConfig.ScopesArray;
+        }
+    }
+}
